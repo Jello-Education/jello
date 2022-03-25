f0670de493f32efe5b4394954dc35e00a441978b
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-03-25 15:31:33 -0300

    feat: preparing for production

--- src/screens/authentication/SignIn.screen.jsx
+++ src/screens/authentication/SignIn.screen.jsx
@@ -1,25 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() => ({
-  section: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 1,
-  },
-}));
-
-const SignIn = () => {
-  const [loading, setLoading] = useState();
-
-  return (
-    <div>
-      <p>SignIn</p>
-    </div>
-  );
-};
+const SignIn = () => (
+  <div>
+    <p>SignIn</p>
+  </div>
+);
 
 export default SignIn;
--- src/screens/authentication/SignUp.screen.jsx
+++ src/screens/authentication/SignUp.screen.jsx
@@ -1,17 +1,5 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() => ({
-  section: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 1,
-  },
-}));
-
 const SignUp = () => {
   <div>
     <p>SignUp</p>
