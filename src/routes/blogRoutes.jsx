2249ffdb2f51271d7fea5e8c3f2ffe52051a4a26
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-05-01 04:28:55 -0300

    feat: creating blog main route

--- src/routes/blogRoutes.jsx
+++ src/routes/blogRoutes.jsx
@@ -3,6 +3,7 @@ import React, { lazy } from 'react';
 import DefaultLayout from '../layout/Default';
 
 const ContentBlog = lazy(() => import('../screens/Blog/ContentBlog'));
+const HomeBlog = lazy(() => import('../screens/Blog/HomeBlog'));
 
 const addCommonProps = (route) => ({
   ...route,
@@ -10,7 +11,10 @@ const addCommonProps = (route) => ({
   layout: DefaultLayout,
 });
 
-const blogRoutes = [{ path: '/blog/content', element: () => <ContentBlog /> }];
+const blogRoutes = [
+  { path: '/blog', element: () => <HomeBlog /> },
+  { path: '/blog/content', element: () => <ContentBlog /> },
+];
 
 const mappedBlogRoutes = blogRoutes.map(addCommonProps);
 
