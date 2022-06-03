8295dec1571a7dbc1edead92d928567c668c67d0
Kassio <kassiomatheus23@gmail.com>
2022-06-02 22:49:39 -0300

    Add pc responsive 50%

--- src/screens/Plans/index.tsx
+++ src/screens/Plans/index.tsx
@@ -1,108 +1,142 @@
 import React from 'react';
 
-import { ContainerInfo, Title, Description, Button, ImageBackground, ContainerPlans, ContainerBox, Box, ButtonBox, TitlePlans, BoxPrincipal, ContainerImage, ImageInfo, TitleInfo, DescriptionInfo, TitleBox, DescriptionBox, TitleBoxPrincipal, DescriptionBoxPrincipal, ContainerTop, ContainerImageInfo, ContainerButton, ContainerBoxs, ContainerBoxInfo } from './style';
-import Header from '../../components/Header';
 import Image from '../../assets/image/Background-Plans.jpg';
 import ImageInfo1 from '../../assets/image/Devices.jpg';
 import ImageInfo2 from '../../assets/image/Offline.png';
+import Header from '../../components/Header';
+import {
+  Box,
+  BoxPrincipal,
+  Button,
+  ButtonBox,
+  ContainerBox,
+  ContainerBoxInfo,
+  ContainerBoxs,
+  ContainerButton,
+  ContainerImage,
+  ContainerImageInfo,
+  ContainerInfo,
+  ContainerPlans,
+  ContainerTop,
+  Description,
+  DescriptionBox,
+  DescriptionBoxPrincipal,
+  DescriptionInfo,
+  ImageBackground,
+  ImageInfo,
+  Title,
+  TitleBox,
+  TitleBoxPrincipal,
+  TitleInfo,
+  TitlePlans,
+} from './style';
 
-const Plans = () => {
-  return (
-    <div>
+const Plans = () => (
+  <div>
+    <Header />
 
-      <Header />
+    <ContainerInfo style={{ backgroundColor: '#DCDCDC' }}>
+      <ContainerTop>
+        <Title>Faça curso online com a gente</Title>
+        <Description>
+          Assine a plataforma Jello e tenha a oportunidade de fazer seu plano de
+          estudo personalizado. Uma plataforma complemente diferente, mas com
+          foco no seu objetivo.
+        </Description>
+        <ContainerButton>
+          <Button>Ver planos</Button>
+        </ContainerButton>
+      </ContainerTop>
 
-      <ContainerInfo style={{ backgroundColor: "#DCDCDC" }}>
+      <ContainerImageInfo>
+        <ImageBackground src={Image} />
+      </ContainerImageInfo>
+    </ContainerInfo>
 
-          <ContainerTop>
-
-            <Title>Faça curso online com a gente</Title>
-            <Description>Assine a plataforma Jello e tenha a oportunidade de fazer seu plano de estudo personalizado. Uma plataforma complemente diferente, mas com foco no seu objetivo.</Description>
-            <ContainerButton>
-              <Button>Ver planos</Button>
-            </ContainerButton>
-
-          </ContainerTop>
-
-          <ContainerImageInfo>
-            <ImageBackground src={Image} />
-          </ContainerImageInfo>
-
-      </ContainerInfo>
-
-      <ContainerPlans>
-        
-        <TitlePlans>
+    <ContainerPlans>
+      <TitlePlans>
         Comece a aprender agora mesmo!
-        <br /> Conheça nossos planos: 
-        </TitlePlans>
+        <br /> Conheça nossos planos:
+      </TitlePlans>
 
-        <ContainerBoxs>
-
-            <ContainerBox>
-
-            <Box>
-              <TitleBox>STANDARD</TitleBox>
-              <DescriptionBox>GRÁTIS</DescriptionBox>
-            </Box>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <ButtonBox>Quero esse!</ButtonBox>
-            </div>
-
-            </ContainerBox>
-
-            <ContainerBox>
-
-            <BoxPrincipal>
-              <TitleBoxPrincipal>ESPINELO</TitleBoxPrincipal>
-              <DescriptionBoxPrincipal>R$ 25,90</DescriptionBoxPrincipal>
-            </BoxPrincipal>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <ButtonBox>Quero esse!</ButtonBox>
-            </div>
-
-            </ContainerBox>
-
-            <ContainerBox>
-
-            <Box>
-              <TitleBox>PALADIUM</TitleBox>
-              <DescriptionBox>R$ 35,90</DescriptionBox>
-            </Box>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <ButtonBox>Quero esse!</ButtonBox>
-            </div>
-
-          </ContainerBox>
-
-        </ContainerBoxs>
-
-      </ContainerPlans>
-
-      <ContainerImage>
-
-        <ContainerBoxInfo>
-          <div style={{ display: "flex", justifyContent: "center" }} >
-            <ImageInfo src={ImageInfo1} />
+      <ContainerBoxs>
+        <ContainerBox>
+          <Box>
+            <TitleBox>STANDARD</TitleBox>
+            <DescriptionBox>GRÁTIS</DescriptionBox>
+          </Box>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <ButtonBox>Quero esse!</ButtonBox>
           </div>
-          <TitleInfo>Assista onde quiser</TitleInfo>
-          <DescriptionInfo>Assista no site, no app do seu celular, ou no <br /> app do seu tablet. Estude onde e <br /> como você preferir.</DescriptionInfo>
-        </ContainerBoxInfo>
+        </ContainerBox>
 
-        <ContainerBoxInfo>
-
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <ImageInfo src={ImageInfo2} />
+        <ContainerBox>
+          <BoxPrincipal>
+            <TitleBoxPrincipal>ESPINELO</TitleBoxPrincipal>
+            <DescriptionBoxPrincipal>R$ 25,90</DescriptionBoxPrincipal>
+          </BoxPrincipal>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <ButtonBox>Quero esse!</ButtonBox>
           </div>
-          <TitleInfo>Economize dados e <br /> assista off-line</TitleInfo>
-          <DescriptionInfo>Já pensou em assistir aulas sem gastar seus <br /> dados móveis? Pois é, aqui na Jello você <br /> pode. É só baixar o nosso app e fazer o <br /> download das suas aulas.</DescriptionInfo>
+        </ContainerBox>
 
-        </ContainerBoxInfo>
+        <ContainerBox>
+          <Box>
+            <TitleBox>PALADIUM</TitleBox>
+            <DescriptionBox>R$ 35,90</DescriptionBox>
+          </Box>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <ButtonBox>Quero esse!</ButtonBox>
+          </div>
+        </ContainerBox>
+      </ContainerBoxs>
+    </ContainerPlans>
 
-      </ContainerImage>
+    <ContainerImage>
+      <ContainerBoxInfo>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <ImageInfo src={ImageInfo1} />
+        </div>
+        <TitleInfo>Assista onde quiser</TitleInfo>
+        <DescriptionInfo>
+          Assista no site, no app do seu celular, ou no <br /> app do seu
+          tablet. Estude onde e <br /> como você preferir.
+        </DescriptionInfo>
+      </ContainerBoxInfo>
 
-    </div>
-  )
-}
+      <ContainerBoxInfo>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <ImageInfo src={ImageInfo2} />
+        </div>
+        <TitleInfo>
+          Economize dados e <br /> assista off-line
+        </TitleInfo>
+        <DescriptionInfo>
+          Já pensou em assistir aulas sem gastar seus <br /> dados móveis? Pois
+          é, aqui na Jello você <br /> pode. É só baixar o nosso app e fazer o{' '}
+          <br /> download das suas aulas.
+        </DescriptionInfo>
+      </ContainerBoxInfo>
+    </ContainerImage>
+  </div>
+);
 
 export default Plans;
--- src/screens/Plans/style.ts
+++ src/screens/Plans/style.ts
@@ -1,30 +1,32 @@
 import styled from 'styled-components';
 
 export const ContainerInfo = styled.div`
-width: 100%;
-height: 95vh;
-display: flex;
-align-items: center;
+  width: 100%;
+  height: 95vh;
+  display: flex;
+  align-items: center;
 
-@media only screen and (max-width: 991px) {
-  display: grid;
-  padding-top: 30px;
-  align-items: flex-start;
-  height: 100%;
-}
-
+  @media only screen and (max-width: 991px) {
+    display: grid;
+    padding-top: 30px;
+    align-items: flex-start;
+    height: 100%;
+  }
 `;
 
 export const ContainerTop = styled.div`
-padding-left: 50px;
-width: 50%;
+  padding-left: 50px;
+  width: 50%;
 
-@media only screen and (max-width: 991px) {
-  padding-left: 0px;
-  padding: 30px;
-  width: 100%;
-}
+  @media only screen and (max-width: 991px) {
+    padding-left: 0px;
+    padding: 30px;
+    width: 100%;
+  }
 
+  @media only screen and (min-width: 2100px) {
+    padding-left: 330px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -45,206 +47,197 @@ export const Title = styled.h1`
     text-shadow: 3px 3px 0px rgba(253, 51, 115, 1);
   }
 
+  @media only screen and (max-width: 1500px) {
+    font-size: 60px;
+    margin-bottom: 15px;
+  }
 `;
 
 export const Description = styled.p`
-font-size: 22px;
-color: #000;
-margin-top: 3%;
-width: 60%;
+  font-size: 22px;
+  color: #000;
+  margin-top: 3%;
+  width: 60%;
 
-@media only screen and (max-width: 991px) {
-  text-align: center;
-  width: 100%;
-  font-size: 18px;
-}
-
+  @media only screen and (max-width: 991px) {
+    text-align: center;
+    width: 100%;
+    font-size: 18px;
+  }
 `;
 
 export const ContainerImageInfo = styled.div`
-width: 50%;
-@media only screen and (max-width: 991px) {
-  width: 100%;
-}
+  width: 50%;
+  @media only screen and (max-width: 991px) {
+    width: 100%;
+  }
 `;
 
 export const Button = styled.button`
-width: 350px;
-border-radius: 70px;
-background: #FD3373;
-color: #fff;
-height: 60px;
-margin-top: 3%;
-border: 0;
-font-size: 18px;
+  width: 350px;
+  border-radius: 70px;
+  background: #fd3373;
+  color: #fff;
+  height: 60px;
+  margin-top: 3%;
+  border: 0;
+  font-size: 18px;
 
-@media only screen and (max-width: 991px) {
-  height: 55px;
+  @media only screen and (max-width: 991px) {
+    height: 55px;
     width: 300px;
     font-size: 14px;
     margin-top: 5%;
   }
-
 `;
 
 export const ImageBackground = styled.img`
-width: 100%;
-height: 95vh;
-object-fit: cover;
+  width: 100%;
+  height: 95vh;
+  object-fit: cover;
 
-@media only screen and (max-width: 991px) {
-  height: 40vh;
-}
-
+  @media only screen and (max-width: 991px) {
+    height: 40vh;
+  }
 `;
 
 export const ContainerPlans = styled.div`
-display: grid;
-justify-content: center;
-align-items: center;
-height: 100%;
-padding: 30px;
-
-
-
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 30px;
 `;
 
 export const TitlePlans = styled.h1`
-text-align: center;
-font-size: 50px;
-margin-top: 3%;
+  text-align: center;
+  font-size: 50px;
+  margin-top: 3%;
 
-@media only screen and (max-width: 991px) {
-  font-size: 18px;
-}
-
+  @media only screen and (max-width: 991px) {
+    font-size: 18px;
+  }
 `;
 
 export const ContainerBox = styled.div`
-margin-right: 3%;
+  margin-right: 3%;
 `;
 
 export const Box = styled.div`
-width: 400px;
-height: 600px;
-border-radius: 50px;
-background: #fff;
-padding: 30px;
-margin-top: 5%;
-box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.72);
--webkit-box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.72);
--moz-box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.72);
-
-@media only screen and (max-width: 991px) {
-  width: 350px;
+  width: 400px;
   height: 600px;
-  box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.50);
--webkit-box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.50);
--moz-box-shadow: 0px 0px 122px -23px rgba(0,0,0,0.50);
-}
+  border-radius: 50px;
+  background: #fff;
+  padding: 30px;
+  margin-top: 5%;
+  box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.72);
+  -webkit-box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.72);
+  -moz-box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.72);
 
+  @media only screen and (max-width: 991px) {
+    width: 350px;
+    height: 600px;
+    box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.5);
+    -webkit-box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 0px 0px 122px -23px rgba(0, 0, 0, 0.5);
+  }
 `;
 
 export const BoxPrincipal = styled.div`
-width: 400px;
-height: 700px;
-border-radius: 50px;
-padding: 30px;
-background: #FD3373;
-margin-top: 10%;
+  width: 400px;
+  height: 700px;
+  border-radius: 50px;
+  padding: 30px;
+  background: #fd3373;
+  margin-top: 10%;
 
-@media only screen and (max-width: 991px) {
-  width: 350px;
-  height: 600px;
-}
-
+  @media only screen and (max-width: 991px) {
+    width: 350px;
+    height: 600px;
+  }
 `;
 
 export const ButtonBox = styled.button`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 300px;
-height: 60px;
-background: #44F9BD;
-border: none;
-margin-top: 10%;
-font-size: 18px;
-border-radius: 50px;
-font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 60px;
+  background: #44f9bd;
+  border: none;
+  margin-top: 10%;
+  font-size: 18px;
+  border-radius: 50px;
+  font-weight: 500;
 
-@media only screen and (max-width: 991px) {
-  margin-top: 5%;
-}
-
+  @media only screen and (max-width: 991px) {
+    margin-top: 5%;
+  }
 `;
 
 export const ContainerImage = styled.div`
-display: flex;
-justify-content: center;
-align-items: flex-start;
-padding: 100px;
-
-@media only screen and (max-width: 991px) {
-  display: grid;
-  padding: 20px;
+  display: flex;
   justify-content: center;
-  align-items: center;
-}
+  align-items: flex-start;
+  padding: 100px;
 
+  @media only screen and (max-width: 991px) {
+    display: grid;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const ImageInfo = styled.img`
-width: 80%;
+  width: 80%;
 `;
 
 export const TitleInfo = styled.h1`
-text-align: center;
-margin-top: 3%;
+  text-align: center;
+  margin-top: 3%;
 
-@media only screen and (max-width: 991px) {
-  font-size: 20px;
-}
-
+  @media only screen and (max-width: 991px) {
+    font-size: 20px;
+  }
 `;
 
 export const DescriptionInfo = styled.p`
-text-align: center;
-margin-top: 1%;
-@media only screen and (max-width: 991px) {
-  font-size: 14px;
-}
+  text-align: center;
+  margin-top: 1%;
+  @media only screen and (max-width: 991px) {
+    font-size: 14px;
+  }
 `;
 
 export const TitleBox = styled.h1`
-font-size: 35px;
-color: #282828;
-text-align: center;
-font-weight: 900;
+  font-size: 35px;
+  color: #282828;
+  text-align: center;
+  font-weight: 900;
 `;
 
 export const DescriptionBox = styled.h1`
-font-size: 40px;
-color: #44F9BD;
-text-align: center;
-font-weight: 900;
-text-shadow: 0px 2px 0px rgba(0,0,0,1);
+  font-size: 40px;
+  color: #44f9bd;
+  text-align: center;
+  font-weight: 900;
+  text-shadow: 0px 2px 0px rgba(0, 0, 0, 1);
 `;
 
-
 export const TitleBoxPrincipal = styled.h1`
-font-size: 35px;
-color: #fff;
-text-align: center;
-font-weight: 900;
+  font-size: 35px;
+  color: #fff;
+  text-align: center;
+  font-weight: 900;
 `;
 
 export const DescriptionBoxPrincipal = styled.h1`
-font-size: 40px;
-color: #44F9BD;
-text-align: center;
-font-weight: 900;
-text-shadow: 0px 2px 0px rgba(0,0,0,1);
+  font-size: 40px;
+  color: #44f9bd;
+  text-align: center;
+  font-weight: 900;
+  text-shadow: 0px 2px 0px rgba(0, 0, 0, 1);
 `;
 
 export const ContainerButton = styled.div`
@@ -255,24 +248,23 @@ export const ContainerButton = styled.div`
 `;
 
 export const ContainerBoxs = styled.div`
-display: flex;
-justify-content: center;
-align-items: flex-end;
-margin-top: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin-top: 5%;
 
-@media only screen and (max-width: 991px) {
-  display: grid;
-}
+  @media only screen and (max-width: 991px) {
+    display: grid;
+  }
 `;
 
 export const ContainerBoxInfo = styled.div`
-display: grid;
-justify-content: center;
-width: 50%;
+  display: grid;
+  justify-content: center;
+  width: 50%;
 
-@media only screen and (max-width: 991px) {
-  width: 100%;
-  margin-top: 3%;
-}
-
+  @media only screen and (max-width: 991px) {
+    width: 100%;
+    margin-top: 3%;
+  }
 `;
